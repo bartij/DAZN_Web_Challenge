eaab3acc501a0d7adc02daa6dc6b9d4a4f9e696a
Bartosz Jurga <barti.jurga@gmail.com>
2018-02-12 21:59:01 +0100

    movie db connected

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import movieDb from 'themoviedb-javascript-library';
+
+movieDb.common.api_key = '73f443fa5350c99236bed025a5030a7d';
 
 class App extends Component {
+  componentDidMount(){
+    movieDb.search.getKeyword({query: 'fight club'},
+      (res) => {
+        const movie = JSON.parse(res).results[0];
+        console.log({movie})
+        movieDb.movies.getById({"id": movie.id }, (response) => {
+          console.log(JSON.parse(response))
+        }, ()=>{})
+      },
+      (err) => {console.log({err})})
+  }
   render() {
     return (
       <div className="App">
